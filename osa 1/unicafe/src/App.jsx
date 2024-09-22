c4236357f139a44d13622a9441494d178c733814
Riku Lauttia <riku.lauttia@gmail.com>
2024-09-22 19:04:40 +0300

    tehtävä ei vielä valmis

--- osa 1/unicafe/src/App.jsx
+++ osa 1/unicafe/src/App.jsx
@@ -1,19 +1,27 @@
 import { useState } from "react";
 
-const Display = (props) => {
-  return <div>{props.counter}</div>;
-};
+const Display = ({ counter }) => <div>{counter}</div>;
 
-const Button = (props) => {
-  return <button onClick={props.handleClick}>{props.text}</button>;
-};
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
 
 const App = () => {
   const [counter, setCounter] = useState(0);
+  console.log("rendering with counter value", counter);
 
-  const increaseByOne = () => setCounter(counter + 1);
-  const decreaseByOne = () => setCounter(counter - 1);
-  const setToZero = () => setCounter(0);
+  const increaseByOne = () => {
+    console.log("increasing, value before", counter);
+    setCounter(counter + 1);
+  };
+  const decreaseByOne = () => {
+    console.log("decreasing, value before", counter);
+    setCounter(counter - 1);
+  };
+  const setToZero = () => {
+    console.log("resetting to zero, value before", counter);
+    setCounter(0);
+  };
 
   return (
     <div>
