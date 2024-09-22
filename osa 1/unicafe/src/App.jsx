8178a399eff09be31b5589bb227694bb68e71be1
Riku Lauttia <riku.lauttia@gmail.com>
2024-09-22 22:51:45 +0300

    tehtävä 1.6 valmis

--- osa 1/unicafe/src/App.jsx
+++ osa 1/unicafe/src/App.jsx
@@ -1,34 +1,25 @@
 import { useState } from "react";
 
-const Display = ({ counter }) => <div>{counter}</div>;
-
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 );
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  console.log("rendering with counter value", counter);
-
-  const increaseByOne = () => {
-    console.log("increasing, value before", counter);
-    setCounter(counter + 1);
-  };
-  const decreaseByOne = () => {
-    console.log("decreasing, value before", counter);
-    setCounter(counter - 1);
-  };
-  const setToZero = () => {
-    console.log("resetting to zero, value before", counter);
-    setCounter(0);
-  };
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button handleClick={increaseByOne} text="plus" />
-      <Button handleClick={setToZero} text="zero" />
-      <Button handleClick={decreaseByOne} text="minus" />
+      <h1>give feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+
+      <h2>statistics</h2>
+      <p>good: {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad: {bad}</p>
     </div>
   );
 };
