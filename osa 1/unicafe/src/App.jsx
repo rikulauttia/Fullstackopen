07108a7e8225affae18123d133c3907fa213d4a4
Riku Lauttia <riku.lauttia@gmail.com>
2024-09-22 22:59:22 +0300

    tehtävä 1.7 valmis

--- osa 1/unicafe/src/App.jsx
+++ osa 1/unicafe/src/App.jsx
@@ -8,6 +8,9 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const all = good + neutral + bad;
+  const average = (good - bad) / all;
+  const positive = (good / all) * 100;
 
   return (
     <div>
@@ -18,8 +21,11 @@ const App = () => {
 
       <h2>statistics</h2>
       <p>good: {good}</p>
-      <p>neutral {neutral}</p>
+      <p>neutral: {neutral}</p>
       <p>bad: {bad}</p>
+      <p>all: {all}</p>
+      <p>average: {average.toFixed(9)}</p>
+      <p>positive: {positive.toFixed(2)}%</p>
     </div>
   );
 };
