01856585ebd595413add054adc196ffa7e6b66ae
Riku Lauttia <riku.lauttia@gmail.com>
2024-09-25 20:47:13 +0300

    teht 2.1

--- osa 2/renderointi_ja_moduulit/src/App.jsx
+++ osa 2/renderointi_ja_moduulit/src/App.jsx
@@ -1,14 +1,44 @@
-const App = (props) => {
-  const { notes } = props;
+import React from "react";
+
+const Header = ({ courseName }) => {
+  return <h1>{courseName}</h1>;
+};
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map((part, index) => (
+      <Part key={index} part={part} />
+    ))}
+  </div>
+);
+
+const Total = ({ parts }) => (
+  <p>
+    Number of exercises {parts.reduce((sum, part) => sum + part.exercises, 0)}
+  </p>
+);
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      { name: "Fundamentals of React", exercises: 10 },
+      { name: "Using props to pass data", exercises: 7 },
+      { name: "State of a component", exercises: 14 },
+    ],
+  };
 
   return (
     <div>
-      <h1>Notes</h1>
-      <ul>
-        <li>{notes[0].content}</li>
-        <li>{notes[1].content}</li>
-        <li>{notes[2].content}</li>
-      </ul>
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
