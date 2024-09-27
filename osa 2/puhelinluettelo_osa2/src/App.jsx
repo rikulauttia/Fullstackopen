722c3ec14226144a62835288c7f5cba136e23199
Riku Lauttia <riku.lauttia@gmail.com>
2024-09-27 20:49:07 +0300

    teht 2.8 valmis

--- osa 2/puhelinluettelo_osa2/src/App.jsx
+++ osa 2/puhelinluettelo_osa2/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -10,14 +11,19 @@ const App = () => {
     if (nameExists) {
       alert(`${newName} is already in the phonebook`);
     } else {
-      const newPerson = { name: newName };
+      const newPerson = { name: newName, number: newNumber };
       setPersons(persons.concat(newPerson));
       setNewName("");
+      setNewNumber("");
     }
   };
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
   };
 
   return (
@@ -28,13 +34,18 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
         {persons.map((person, index) => (
-          <div key={index}>{person.name} </div>
+          <div key={index}>
+            {person.name} {person.number}{" "}
+          </div>
         ))}
       </div>
     </div>
