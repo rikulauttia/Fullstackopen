4df50f01730d00e870286d88f8a49e9ccfd4994d
Riku Lauttia <riku.lauttia@gmail.com>
2024-09-28 18:17:35 +0300

    update

--- osa 2/puhelinluettelo_osa2/src/main.jsx
+++ osa 2/puhelinluettelo_osa2/src/main.jsx
@@ -1,9 +1,5 @@
-import axios from "axios";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
-
-const promise = axios.get("http://localhost:3001/persons");
-console.log(promise);
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
