3b858ce214d9caa7773628efc277963501ef01ff
Riku Lauttia <riku.lauttia@gmail.com>
2024-10-15 20:55:44 +0300

    teht 4.4 valmis

--- osa 4/tests/list_helper.test.js
+++ osa 4/tests/list_helper.test.js
@@ -1,5 +1,16 @@
 const listHelper = require('../utils/list_helper')
 
+const listWithOneBlog = [
+	{
+		_id: '5a422aa71b54a676234d17f8',
+		title: 'Go To Statement Considered Harmful',
+		author: 'Edsger W. Dijkstra',
+		url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+		likes: 5,
+		__v: 0
+	}
+]
+
 const blogs = [
   {
     _id: "5a422a851b54a676234d17f7",
@@ -58,3 +69,20 @@ test('dummy returns one', () => {
   expect(result).toBe(1)
 })
 
+describe ('total likes', () => {
+  test('of empty lists is zero', () => {
+    const result = listHelper.totalLikes([])
+    expect(result).toBe(0)
+  })
+
+  test('when list has only one blog equals likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(5)
+  })
+
+  test('of bigger lists is calculated right', () => {
+    const result = listHelper.totalLikes(blogs)
+    expect(result).toBe(36)
+  })
+
+})
