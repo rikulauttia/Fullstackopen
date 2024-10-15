ef8b3c25f3f2715da17a1da2cd9f2a8c2a93fc41
Riku Lauttia <riku.lauttia@gmail.com>
2024-10-15 21:41:41 +0300

    teht 4.5 valmis

--- osa 4/tests/list_helper.test.js
+++ osa 4/tests/list_helper.test.js
@@ -86,3 +86,28 @@ describe ('total likes', () => {
   })
 
 })
+
+describe ('favorite blog', () => {
+  test('of empty lists is null', () => {
+    const result = listHelper.favoriteBlog([])
+    expect(result).toEqual(null)
+  })
+
+  test('when list has only one blog equals that', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    expect(result).toEqual({
+      title: 'Go To Statement Considered Harmful',
+		  author: 'Edsger W. Dijkstra',
+		  likes: 5
+    })
+  })
+
+  test('of bigger lists is founded', () => {
+    const result = listHelper.favoriteBlog(blogs)
+    expect(result).toEqual({
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      likes: 12
+    })
+  })
+})
--- osa 4/utils/list_helper.js
+++ osa 4/utils/list_helper.js
@@ -6,6 +6,22 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+
+  mostLikedBlog = blogs.reduce((favorite, blog) => {
+    return blog.likes > favorite.likes ? blog : favorite
+  })
+
+  return {
+    title: mostLikedBlog.title,
+    author: mostLikedBlog.author,
+    likes: mostLikedBlog.likes
+  }
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
