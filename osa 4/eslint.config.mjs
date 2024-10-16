f3f61acb08a749a879a391ffac9c1649e6837d19
Riku Lauttia <riku.lauttia@gmail.com>
2024-10-16 10:40:35 +0300

    upgrade

--- osa 4/eslint.config.mjs
+++ osa 4/eslint.config.mjs
@@ -6,27 +6,30 @@ import stylisticJs from '@stylistic/eslint-plugin-js';
 export default [
   js.configs.recommended,
   {
-    files: ["**/*.js"],
+    files: ['**/*.js'],
     languageOptions: {
-      sourceType: "commonjs",
+      sourceType: 'commonjs',
       globals: {
         ...globals.node,
+        ...globals.browser,
+        ...globals.jest,
+        ...globals.es2021
       },
-      ecmaVersion: "latest",
+      ecmaVersion: 'latest',
     },
     plugins: {
-      "@stylistic/js": stylisticJs,
+      '@stylistic/js': stylisticJs,
     },
     rules: {
-      "@stylistic/js/indent": ["error", 2],
-      "@stylistic/js/linebreak-style": ["error", "unix"],
-      "@stylistic/js/quotes": ["error", "single"],
-      "@stylistic/js/semi": ["error", "never"],
-      eqeqeq: "error",
-      "no-trailing-spaces": "error",
-      "object-curly-spacing": ["error", "always"],
-      "arrow-spacing": ["error", { before: true, after: true }],
-      "no-console": "off",
+      '@stylistic/js/indent': ['error', 'tab'],
+      '@stylistic/js/linebreak-style': ['error', 'unix'],
+      '@stylistic/js/quotes': ['error', 'single'],
+      '@stylistic/js/semi': ['error', 'never'],
+      eqeqeq: 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'no-console': 'off',
     },
   },
-];
+]
--- osa 4/utils/list_helper.js
+++ osa 4/utils/list_helper.js
@@ -1,47 +1,47 @@
 const dummy = (blogs) => {
-  return 1
+	return 1
 }
 
 const totalLikes = (blogs) => {
-  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+	return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
 const favoriteBlog = (blogs) => {
-  if (blogs.length === 0) {
-    return null
-  }
+	if (blogs.length === 0) {
+		return null
+	}
 
-  mostLikedBlog = blogs.reduce((favorite, blog) => {
-    return blog.likes > favorite.likes ? blog : favorite
-  })
+	mostLikedBlog = blogs.reduce((favorite, blog) => {
+		return blog.likes > favorite.likes ? blog : favorite
+	})
 
-  return {
-    title: mostLikedBlog.title,
-    author: mostLikedBlog.author,
-    likes: mostLikedBlog.likes
-  }
+	return {
+		title: mostLikedBlog.title,
+		author: mostLikedBlog.author,
+		likes: mostLikedBlog.likes
+	}
 }
 
 const mostBlogs = (blogs) => {
-  if (blogs.length === 0) {
-    return null
-  }
+	if (blogs.length === 0) {
+		return null
+	}
 
-  const authorBlogCounts = blogs.reduce((counts, blog) => {
-    counts[blog.author] = (counts[blog.author] || 0) + 1
-    return counts
-  }, {})
+	const authorBlogCounts = blogs.reduce((counts, blog) => {
+		counts[blog.author] = (counts[blog.author] || 0) + 1
+		return counts
+	}, {})
 
-  const mostBlogsAuthor = Object.keys(authorBlogCounts).reduce((maxAuthor, author) => {
-    return authorBlogCounts[author] > authorBlogCounts[maxAuthor] ? author : maxAuthor
-  })
+	const mostBlogsAuthor = Object.keys(authorBlogCounts).reduce((maxAuthor, author) => {
+		return authorBlogCounts[author] > authorBlogCounts[maxAuthor] ? author : maxAuthor
+	})
 
-  return {
-    author: mostBlogsAuthor,
-    blogs: authorBlogCounts[mostBlogsAuthor]
-  }
+	return {
+		author: mostBlogsAuthor,
+		blogs: authorBlogCounts[mostBlogsAuthor]
+	}
 }
 
 module.exports = {
-  dummy, totalLikes, favoriteBlog, mostBlogs
+	dummy, totalLikes, favoriteBlog, mostBlogs
 }
