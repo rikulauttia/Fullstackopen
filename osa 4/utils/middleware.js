13eacf522013cec2adede7c16b9837cdfe31ce7b
Riku Lauttia <riku.lauttia@gmail.com>
2024-10-22 12:29:47 +0300

    teht 4.20 valmis

--- osa 4/utils/middleware.js
+++ osa 4/utils/middleware.js
@@ -12,4 +12,16 @@ const errorHandler = (error, request, response, next) => {
 	}
 	next(error)
 }
-module.exports = errorHandler
+
+const tokenExtractor = (request, response, next) => {
+	const authorization = request.get('authorization')
+	if (authorization && authorization.startsWith('Bearer ')) {
+		request.token = authorization.replace('Bearer ', '')
+	} else {
+		request.token = null
+	}
+
+	next()
+}
+
+module.exports = { errorHandler, tokenExtractor }
