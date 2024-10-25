0fced0432713f1113567bf94884a51dc84610b7a
Riku Lauttia <riku.lauttia@gmail.com>
2024-10-25 17:26:49 +0300

    Update

--- osa 5/src/App.jsx
+++ osa 5/src/App.jsx
@@ -3,11 +3,14 @@ import {
   useState,
 } from 'react';
 
-import Blog from './components/Blog';
 import blogService from './services/blogs';
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
+
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -15,12 +18,35 @@ const App = () => {
     )  
   }, [])
 
+  const handleLogin = (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+  }
+
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      <form>
+        <div>
+          username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+          <button type="submit">login</button>
+      </form>
     </div>
   )
 }
