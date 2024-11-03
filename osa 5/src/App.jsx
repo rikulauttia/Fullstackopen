d2f801ad62f4bc79a0e6a6a02723be5e094cfc04
Riku Lauttia <riku.lauttia@gmail.com>
2024-11-03 17:02:02 +0200

    teht 5.10 valmis

--- osa 5/src/App.jsx
+++ osa 5/src/App.jsx
@@ -100,11 +100,14 @@ const App = () => {
         <BlogForm createBlog={addBlog} />
       </Togglable>
 
-      {blogs.map(blog => (
-        <Blog key={blog.id} blog={blog} updateBlog={updateBlog}/>
+      {blogs
+      .slice()
+      .sort((a, b) => b.likes - a.likes)
+      .map(blog => (
+        <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />
       ))}
-    </div>
-  );
+  </div>
+);
 }
 
 export default App
