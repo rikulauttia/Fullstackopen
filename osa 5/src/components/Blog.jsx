36a81bb7d74a2c048d758c115b7b381af242cadb
Riku Lauttia <riku.lauttia@gmail.com>
2024-11-03 13:18:28 +0200

    teht 5.7 valmis

--- osa 5/src/components/Blog.jsx
+++ osa 5/src/components/Blog.jsx
@@ -1,7 +1,33 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react';
 
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  return (
+    <div style={blogStyle}>
+      <div> 
+        {blog.title} {blog.author}
+        <button onClick={toggleVisibility}>{visible ? 'hide' : 'view'}</button>
+      </div>
+      {visible && (
+        <div>
+          <p>{blog.url}</p>
+          <p>likes {blog.likes} <button>like</button></p>
+          <p>{blog.user.name}</p>
+        </div>
+      )}
+    </div>  
+  )
+}
 export default Blog
