aac7efc6f9cffc2664472c4c1feb4379ba3d7e5d
Riku Lauttia <riku.lauttia@gmail.com>
2024-11-08 01:27:25 +0200

    teht 5.16 valmis

--- osa 5/src/components/Blog.test.jsx
+++ osa 5/src/components/Blog.test.jsx
@@ -4,7 +4,20 @@ import {
 } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
+import blogService from '../services/blogs';
 import Blog from './Blog';
+
+// Mock blogService with default export and the update method
+vi.mock('../services/blogs', async (importOriginal) => {
+    const actual = await importOriginal();
+    return {
+      ...actual,
+      default: {
+        ...actual.default,
+        update: vi.fn(),
+      },
+    };
+});
 
 test('renders title and author', () => {
 	const blog = {
@@ -15,7 +28,6 @@ test('renders title and author', () => {
 		url: 'example.com'
 	}
 
-    const mockHandler = vi.fn()
 
 	render(<Blog blog={blog} />)
 	const titleauthor = screen.getByText('Riku is King rtlaut')
@@ -51,4 +63,37 @@ test('clicking the view button shows also url, likes and user', async () => {
 	
 	const name = screen.queryByText('Riku')
 	expect(name).toBeDefined()
+  })
+
+  test('when like button is pressed two times', async () => {
+      const blog = {
+          user: {
+              name: 'Riku'
+            },
+            author: 'rtlaut',
+            title: 'Riku is King',
+            likes: 100,
+            url: 'example.com'
+        }
+
+        const mockHandler = vi.fn();
+
+        render(
+            <Blog blog={blog} updateBlog={mockHandler}/>
+        )
+        
+        const user = userEvent.setup()
+        const button = screen.getByText('view')
+        await user.click(button)
+        
+        blogService.update.mockResolvedValue({
+            ...blog,
+            likes: blog.likes + 1,
+        });
+
+        const likeButton = screen.getByText('like')
+        await user.click(likeButton)
+        await user.click(likeButton)
+
+        expect(mockHandler).toHaveBeenCalledTimes(2);
   })
