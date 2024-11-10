e5ebf8285017298a2fd372cd382881b427ff9e63
Riku Lauttia <riku.lauttia@gmail.com>
2024-11-10 12:44:46 +0200

    teht 6.2 valmis

--- osa 6/src/reducers/reducer.js
+++ osa 6/src/reducers/reducer.js
@@ -8,13 +8,13 @@ const counterReducer = (state = initialState, action) => {
   console.log(action)
   switch (action.type) {
     case 'GOOD':
-      return {good: state.good +1, ok: state.ok, bad: state.bad}
+      return { good: state.good + 1, ok: state.ok, bad: state.bad }
     case 'OK':
-      return {good: state.good, ok: state.ok +1, bad: state.bad}
+      return { good: state.good, ok: state.ok + 1, bad: state.bad }
     case 'BAD':
-      return {good: state.good, ok: state.ok, bad: state.bad + 1 }
+      return { good: state.good, ok: state.ok, bad: state.bad + 1 }
     case 'ZERO':
-      return {good: 0, ok: 0, bad: 0}
+      return { good: 0, ok: 0, bad: 0 }
     default: return state
   }
 }
