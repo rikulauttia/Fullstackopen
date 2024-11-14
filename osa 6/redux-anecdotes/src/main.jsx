45049468aa11e3fefaccd09b5c96c62d21c9cd17
Riku Lauttia <riku.lauttia@gmail.com>
2024-11-14 20:55:38 +0200

    teht 6.10 valmis

--- osa 6/redux-anecdotes/src/main.jsx
+++ osa 6/redux-anecdotes/src/main.jsx
@@ -1,17 +1,8 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { combineReducers, createStore } from "redux";
 
 import App from "./App";
-import anecdotesReducer from "./reducers/anecdoteReducer";
-import filterReducer from "./reducers/filterReducer";
-
-const reducer = combineReducers({
-  anecdotes: anecdotesReducer,
-  filter: filterReducer,
-});
-
-const store = createStore(reducer);
+import store from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
