22a8508f8769edf2c73f92e347c5d3cd8c06445e
Riku Lauttia <riku.lauttia@gmail.com>
2024-11-16 14:24:02 +0200

    teht 6.14 valmis

--- osa 6/redux-anecdotes/src/store.js
+++ osa 6/redux-anecdotes/src/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import anecdotesReducer from "./reducers/anecdoteReducer";
+import anecdotesReducer, { appendAnecdote } from "./reducers/anecdoteReducer";
 import filterReducer from "./reducers/filterReducer";
 import notificationReducer from "./reducers/notificationReducer";
+import anecdoteService from "./services/anecdotes";
 
 const store = configureStore({
   reducer: {
@@ -11,5 +12,11 @@ const store = configureStore({
     notification: notificationReducer,
   },
 });
+
+anecdoteService.getAll().then((anecdotes) =>
+  anecdotes.forEach((anecdote) => {
+    store.dispatch(appendAnecdote(anecdote));
+  })
+);
 
 export default store;
