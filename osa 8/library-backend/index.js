21355eddbbed219aee18b4e06242a448642346a3
Riku Lauttia <riku.lauttia@gmail.com>
2024-12-26 20:33:26 +0200

    exercise 8.5 completed

--- osa 8/library-backend/index.js
+++ osa 8/library-backend/index.js
@@ -113,7 +113,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -123,10 +123,18 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
+      let filteredBooks = books;
       if (args.author) {
-        return books.filter((book) => book.author === args.author);
+        filteredBooks = filteredBooks.filter(
+          (book) => book.author === args.author
+        );
       }
-      return books;
+      if (args.genre) {
+        filteredBooks = filteredBooks.filter((book) =>
+          book.genres.includes(args.genre)
+        );
+      }
+      return filteredBooks;
     },
     allAuthors: () => {
       return authors.map((author) => ({
