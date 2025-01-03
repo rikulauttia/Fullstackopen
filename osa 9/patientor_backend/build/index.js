a6d885a67d2372418676446a35da3913959e2ad7
Riku Lauttia <riku.lauttia@gmail.com>
2025-01-03 22:55:31 +0200

    exercise 9.9 completed

--- osa 9/patientor_backend/build/index.js
+++ osa 9/patientor_backend/build/index.js
@@ -3,8 +3,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 const PORT = 3001;
 app.get("/api/ping", (_req, res) => {
