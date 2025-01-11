33e11d0456f52821732bde117f0f8f1d3c6972a9
Riku Lauttia <riku.lauttia@gmail.com>
2025-01-12 00:42:05 +0200

    exercise 9.17 completed

---
+++ osa 9/flight_diary_frontend/src/components/Entries.tsx
@@ -0,0 +1,25 @@
+import { DiaryEntry } from "../types";
+
+interface EntriesTypes {
+  entries: DiaryEntry[];
+}
+
+const Entries = ({ entries }: EntriesTypes) => {
+  return (
+    <div>
+      <h1>Diary Entries</h1>
+      {entries.map((entry) => {
+        return (
+          <div key={entry.id}>
+            <h3>{entry.date}</h3>
+            <p>visibility: {entry.visibility}</p>
+            <p>weather: {entry.weather}</p>
+            <br></br>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Entries;
